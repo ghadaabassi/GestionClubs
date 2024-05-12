1b71b1b5150f63d44df318bb14e41317e2ab6ab6
ghada <gh.abassi01@gmail.com>
2024-05-12 17:01:48 +0100

    camelCase

--- GestionClub/src/main/java/com/fullstack/tpspring/Controllers/ClubController.java
+++ GestionClub/src/main/java/com/fullstack/tpspring/Controllers/ClubController.java
@@ -9,27 +9,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/Clubs")
 @AllArgsConstructor
 public class ClubController {
-    IGestionClub IGestionClub;
+    IGestionClub iGestionClub;
 
     @PostMapping("/add")
     public Club addClub(@RequestBody Club Club) {
-        return IGestionClub.addClub(Club);
+        return iGestionClub.addClub(Club);
     }
 
     @GetMapping("get/{id}")
     public Club getById(@PathVariable int id) {
-        return IGestionClub.getClubById(id);
+        return iGestionClub.getClubById(id);
     }
 
     @PutMapping("/update/{id}")
     public Club updateClub(@PathVariable int id, @RequestBody Club Club) {
         Club.setId(id);
-        return IGestionClub.updateClub(Club);
+        return iGestionClub.updateClub(Club);
     }
 
     @DeleteMapping("/delete/{id}")
     public void deleteClub(@PathVariable int id) {
-        IGestionClub.deleteClub(id);
+        iGestionClub.deleteClub(id);
     }
 
 }
--- GestionClub/src/main/java/com/fullstack/tpspring/Controllers/ParticipantController.java
+++ GestionClub/src/main/java/com/fullstack/tpspring/Controllers/ParticipantController.java
@@ -3,37 +3,34 @@ package com.fullstack.tpspring.Controllers;
 import com.fullstack.tpspring.entities.Participant;
 import com.fullstack.tpspring.service.IGestionParticipant;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/participants")
 @AllArgsConstructor
 public class ParticipantController {
 
-    IGestionParticipant IGestionParticipant;
+    IGestionParticipant iGestionParticipant;
 
     @PostMapping("/add")
     public Participant addParticipant(@RequestBody Participant participant) {
-        return IGestionParticipant.addParticipant(participant);
+        return iGestionParticipant.addParticipant(participant);
     }
 
     @GetMapping("get/{id}")
     public Participant getById(@PathVariable int id) {
-        return IGestionParticipant.getPaticipantById(id);
+        return iGestionParticipant.getPaticipantById(id);
     }
 
     @PutMapping("/update/{id}")
     public Participant updateParticipant(@PathVariable int id, @RequestBody Participant participant) {
         participant.setId(id);
-        return IGestionParticipant.updateParticipant(participant);
+        return iGestionParticipant.updateParticipant(participant);
     }
 
     @DeleteMapping("/delete/{id}")
     public void deleteParticipant(@PathVariable int id) {
-        IGestionParticipant.deleteParticipant(id);
+        iGestionParticipant.deleteParticipant(id);
     }
 
 
